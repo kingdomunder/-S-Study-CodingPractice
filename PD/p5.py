54f573a051e406b7f710d1ec91ee092116af92b3
kingdomunder <king4525@gmail.com>
2021-06-26 17:09:42 +0900

    p5.1

--- PD/p5.py
+++ PD/p5.py
@@ -1,13 +1,10 @@
 # 문자열 내 p와 y의 개수 / 대소문자 무관
 
-S = "oooo"
+S = "pPoooyY"
 
 def solution(s):
-    if s.count("p")+s.count("P") == s.count("y")+s.count("Y") or s.count("p")+s.count("P")+s.count("y")+s.count("Y") == 0 :
-        return True
-    else:
-        return False
-
+    answer = bool(s.count("p")+s.count("P") == s.count("y")+s.count("Y") or s.count("p")+s.count("P")+s.count("y")+s.count("Y") == 0)
+    return answer
 print(solution(S))
 
 
