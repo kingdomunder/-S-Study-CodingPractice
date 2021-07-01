2590fcd20df7687bb975b217fed0140fc8578f13
kingdomunder <king4525@gmail.com>
2021-07-01 19:26:53 +0900

    p6.1(.remove->.append)

--- PD/p6.py
+++ PD/p6.py
@@ -1,4 +1,3 @@
-
 # 6.같은 숫자는 싫어 / 연속되는 숫자는 하나만 남기고 모두 제거 / 제거되더라도 남은 숫자들의 배열 순서는 유지 
 
 arr1 = [1,1,3,3,0,1,1]
@@ -6,10 +5,18 @@ arr2 = [4,4,4,3,3]
 
 def solution(arr):
     answer = []
+    answer.append(arr[0])
+    i = 0
     for x in arr:
-        if x == arr[arr.index(x)+1]:
-            arr.remove(x)
-    answer = arr
+        if x != answer[i]:
+            answer.append(x)           
+            i = i + 1
     return answer
 
-print(solution(arr2))
+print(solution(arr1))
+
+
+
+
+
+
