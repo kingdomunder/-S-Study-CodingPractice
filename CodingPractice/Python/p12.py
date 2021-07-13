d64cadc43a2529e0637c4fbe75c4e73def9ecc94
kingdomunder <king4525@gmail.com>
2021-07-14 08:52:10 +0900

    21/07/12 10. (정수의 배열 arr)의 평균값 구하기 / 거품

---
+++ CodingPractice/Python/p12.py
@@ -0,0 +1,15 @@
+# 12. 짝수와 홀수 / 정수 num 이 짝수이면 "Even" , 홀수이면 "Odd" 를 반환하기
+
+num1 = 3
+num2 = 4
+
+def solution(num):
+    answer = 'even' if num%2 == 0 else 'Odd' 
+    return answer
+
+
+print(solution(num1))
+
+# 한 줄로 풀어보기
+# if / else 한 줄로, 이게 되네 - 실험 성공
+
